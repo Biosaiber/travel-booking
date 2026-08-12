f36fea95ae892c771250da25c4ea672dc42e0c37
isubfox <samuel.liska@gmail.com>
2026-08-12 14:10:20 +0200

    feat: created travel model

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,28 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { BookingService } from '../booking.service';
 
+interface TravelModel {
+  fromCountry: string;
+  toCountry: string;
+}
+
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.css',
 })
+
+
 export class HomeComponent implements OnInit {
   countries: string[] = [];
   bookingService = inject(BookingService);
+  travelModel: TravelModel = {
+    fromCountry: '',
+    toCountry: ''
+  };
   ngOnInit(): void {
     this.countries = this.bookingService.getCountries();
   }
-  
+
 }
