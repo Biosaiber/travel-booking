fb6a402eb664cbe8b6c35d3e21063a6aa16d3506
isubfox <samuel.liska@gmail.com>
2026-08-14 12:26:30 +0200

    feat: initialization hotel-booking.component

---
+++ src/app/hotel-booking/hotel-booking.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hotel-booking',
+  imports: [],
+  templateUrl: './hotel-booking.html',
+  styleUrl: './hotel-booking.css',
+})
+export class HotelBooking {
+
+}
