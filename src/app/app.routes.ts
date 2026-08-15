789b45ca6f1b7be4c6f79663d4ad7fc3f075de7c
isubfox <samuel.liska@gmail.com>
2026-08-15 22:15:29 +0200

    feat: routing setup file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FlightBooking } from './flight-booking/flight-booking';
+import { HotelBooking } from './hotel-booking/hotel-booking';
+import { Summary } from './summary/summary';
+import { PageNotFound } from './page-not-found/page-not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        redirectTo: "home",
+        pathMatch: 'full'
+
+    },
+    {
+        path: "home",
+        component: HomeComponent,
+
+    },
+    {
+        path: "flight-booking",
+        component: FlightBooking
+    },
+    {
+        path: "hotel-booking",
+        component: HotelBooking
+    },
+    {
+        path: "summary",
+        component: Summary
+    },
+    {
+        path: "**",
+        component: PageNotFound
+    }
+];
