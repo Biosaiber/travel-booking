425f612d7ee07ab426a36425a5c5020bad8969ef
isubfox <samuel.liska@gmail.com>
2026-08-16 22:27:48 +0200

    feat: init ReactiveFormsModule and declaration of flightForm form

--- src/app/flight-booking/flight-booking.ts
+++ src/app/flight-booking/flight-booking.ts
@@ -2,10 +2,11 @@ import { Component, OnInit, inject } from '@angular/core';
 import { BookingService } from '../booking.service';
 import { TravelSelection } from '../booking.service';
 import { Flight } from '../models/flight.interface';
+import { ReactiveFormsModule, AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-flight-booking',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './flight-booking.html',
   styleUrl: './flight-booking.css',
 })
@@ -13,6 +14,32 @@ export class FlightBooking implements OnInit {
   bookingService = inject(BookingService);
   journey!: TravelSelection;
   availableFlights!: Flight[];
+  flightForm = new FormGroup({
+    flightId: new FormControl("", [Validators.required]),
+    customerName: new FormControl("", [Validators.required]),
+    travelers: new FormControl("", [Validators.required, Validators.min(1)]),
+    departureDate: new FormControl("", [Validators.required, this.noPastDateValidator]),
+    returnDate: new FormControl("", [Validators.required])
+  });
+  // method for departureDate
+  noPastDateValidator(control: AbstractControl) {
+    const conValue = control.value;
+    const today = new Date().toISOString().split('T')[0];
+    if (conValue < today) {
+      return { pastDate: true }
+    } else {
+      return null
+    }
+  }
+  dateOrderValidator(control: AbstractControl) {
+    const departureDate = this.flightForm.controls.departureDate.value;
+    const returnDate = this.flightForm.controls.returnDate.value;
+
+/*    TU SOM SKONCIL: robim metodu pre validaciu datumov aby returnDate nebol neskorsi ako departureDate 
+      if (returnDate > departureDate) {
+      return 
+    } */
+  }
   ngOnInit(): void {
     console.log('INIT');
 
@@ -26,4 +53,7 @@ export class FlightBooking implements OnInit {
 
     console.log('availableFlights:', this.availableFlights);
   }
+
+
+  // method for aarivalDate
 }
