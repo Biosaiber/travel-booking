9dd9a10f37128f05f3c61d00c48c7de712c719b4
isubfox <samuel.liska@gmail.com>
2026-08-16 14:02:26 +0200

    feat: get availabe flight to the flight-booking component

--- src/app/flight-booking/flight-booking.ts
+++ src/app/flight-booking/flight-booking.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { BookingService } from '../booking.service';
+import { TravelSelection } from '../booking.service';
+import { Flight } from '../models/flight.interface';
 
 @Component({
   selector: 'app-flight-booking',
@@ -6,6 +9,21 @@ import { Component } from '@angular/core';
   templateUrl: './flight-booking.html',
   styleUrl: './flight-booking.css',
 })
-export class FlightBooking {
+export class FlightBooking implements OnInit {
+  bookingService = inject(BookingService);
+  journey!: TravelSelection;
+  availableFlights!: Flight[];
+  ngOnInit(): void {
+    console.log('INIT');
 
+    this.journey = this.bookingService.getTravelSelection();
+    console.log('journey:', this.journey);
+
+    this.availableFlights = this.bookingService.getFlights(
+      this.journey.fromCountry,
+      this.journey.toCountry
+    );
+
+    console.log('availableFlights:', this.availableFlights);
+  }
 }
