4e98f805dd6978b3f15c2870d54f7b51cd50c1d2
isubfox <samuel.liska@gmail.com>
2026-08-18 20:24:35 +0200

    feat: html error messages and clearence of console.logs

--- src/app/flight-booking/flight-booking.ts
+++ src/app/flight-booking/flight-booking.ts
@@ -16,14 +16,21 @@ export class FlightBooking implements OnInit {
   availableFlights!: Flight[];
   flightForm = new FormGroup({
     flightId: new FormControl("", [Validators.required]),
-    customerName: new FormControl("", [Validators.required]),
+    customerName: new FormControl("", [Validators.required, Validators.minLength(3)]),
     travelers: new FormControl("", [Validators.required, Validators.min(1)]),
     departureDate: new FormControl("", [Validators.required, this.noPastDateValidator]),
     returnDate: new FormControl("", [Validators.required])
   }, [this.dateOrderValidator]);
+
   noPastDateValidator(control: AbstractControl) {
     const conValue = control.value;
+
+    if (!conValue) {
+      return null;
+    }
+
     const today = new Date().toISOString().split('T')[0];
+
     if (conValue < today) {
       return { pastDate: true }
     } else {
@@ -33,30 +40,27 @@ export class FlightBooking implements OnInit {
   dateOrderValidator(control: AbstractControl) {
     const departureDate = control.get('departureDate')?.value;
     const returnDate = control.get('returnDate')?.value;
-console.log('departure:', departureDate);
-  console.log('return:', returnDate);
     if (!departureDate || !returnDate) {
       return null;
     }
 
     if (returnDate <= departureDate) {
-       console.log('DATE ERROR!');
       return { pastReturnDate: true };
     }
 
     return null;
   }
   ngOnInit(): void {
-    console.log('INIT');
 
     this.journey = this.bookingService.getTravelSelection();
-    console.log('journey:', this.journey);
 
     this.availableFlights = this.bookingService.getFlights(
       this.journey.fromCountry,
       this.journey.toCountry
     );
 
-    console.log('availableFlights:', this.availableFlights);
+  }
+  onSubmit() {
+
   }
 }
