03e7815c023c30005fc0f70b3cc527069b7e7d61
isubfox <samuel.liska@gmail.com>
2026-08-24 14:06:03 +0200

    feat: complete hotel booking and summary flow

--- src/app/summary/summary.ts
+++ src/app/summary/summary.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { BookingService } from '../booking.service';
+import { BookingDetails } from '../models/booking.interface';
 
 @Component({
   selector: 'app-summary',
@@ -6,6 +8,27 @@ import { Component } from '@angular/core';
   templateUrl: './summary.html',
   styleUrl: './summary.css',
 })
-export class Summary {
+export class Summary implements OnInit {
+  bookingService = inject(BookingService);
+  summaryDetails?: BookingDetails;
+  totalCost?: number;
+  ngOnInit(): void {
+    this.summaryDetails = this.bookingService.getBookingDetails();
+
+        if(!this.summaryDetails?.departureDate || !this.summaryDetails?.returnDate) {
+      return;
+    }
+    const departureDate: Date = new Date(this.summaryDetails?.departureDate);
+    const returnDate: Date = new Date(this.summaryDetails?.returnDate);
+    const travelPeriod: number =  Number(returnDate) - Number(departureDate);
+    const days: number = travelPeriod / 1000 / 60 / 60 / 24;
+
+    if (!this.summaryDetails.flight?.price || !this.summaryDetails.travelers || !this.summaryDetails.hotel?.price || !this.summaryDetails.rooms) {
+      return;
+    }
+
+    this.totalCost = this.summaryDetails.flight.price * this.summaryDetails.travelers + this.summaryDetails.hotel.price * this.summaryDetails.rooms * days;
+
+  }
 
 }
