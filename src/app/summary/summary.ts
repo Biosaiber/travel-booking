70e53695b54a08c631aafddc5e1fdaf857c4e087
isubfox <samuel.liska@gmail.com>
2026-08-25 12:26:21 +0200

    refactor: store total cost in booking details

--- src/app/summary/summary.ts
+++ src/app/summary/summary.ts
@@ -11,24 +11,28 @@ import { BookingDetails } from '../models/booking.interface';
 export class Summary implements OnInit {
   bookingService = inject(BookingService);
   summaryDetails?: BookingDetails;
-  totalCost?: number;
   ngOnInit(): void {
     this.summaryDetails = this.bookingService.getBookingDetails();
 
-        if(!this.summaryDetails?.departureDate || !this.summaryDetails?.returnDate) {
+    if (!this.summaryDetails?.departureDate || !this.summaryDetails?.returnDate) {
       return;
     }
-    const departureDate: Date = new Date(this.summaryDetails?.departureDate);
-    const returnDate: Date = new Date(this.summaryDetails?.returnDate);
-    const travelPeriod: number =  Number(returnDate) - Number(departureDate);
+    const departureDate: Date = new Date(this.summaryDetails.departureDate);
+    const returnDate: Date = new Date(this.summaryDetails.returnDate);
+    const travelPeriod: number = Number(returnDate) - Number(departureDate);
     const days: number = travelPeriod / 1000 / 60 / 60 / 24;
 
     if (!this.summaryDetails.flight?.price || !this.summaryDetails.travelers || !this.summaryDetails.hotel?.price || !this.summaryDetails.rooms) {
       return;
     }
 
-    this.totalCost = this.summaryDetails.flight.price * this.summaryDetails.travelers + this.summaryDetails.hotel.price * this.summaryDetails.rooms * days;
+    const totalCost: number = this.summaryDetails.flight.price * this.summaryDetails.travelers + this.summaryDetails.hotel.price * this.summaryDetails.rooms * days;
 
+    const priceSummary: Partial<BookingDetails> = {
+      totalCost
+    }
+    this.bookingService.updateBookingDetails(priceSummary);
+    this.summaryDetails = this.bookingService.getBookingDetails();
   }
 
 }
